6656bd8585b7555f707ba249b72b579685d31b7b
Hafezd El Daffa <hafezdeldaffa9@gmail.com>
2021-10-25 22:16:15 +0700

    changes

--- provinsi.js
+++ provinsi.js
@@ -1,19 +1,18 @@
-const fetch = require('cross-fetch');
+const getData = async () => {
+  try {
+    const data = await fetch('https://api.kawalcorona.com/indonesia/provinsi');
+    return data.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-async function getApi() {
-  fetch('https://api.kawalcorona.com/indonesia/provinsi')
-    .then((res) => {
-      if (res.status >= 400) {
-        throw new Error('Bad response from server');
-      }
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
-
-getApi();
+const renderData = async () => {
+  try {
+    const getApi = await getData();
+    console.log(getApi);
+  } catch (error) {
+    console.error(error);
+  }
+};
+renderData();
